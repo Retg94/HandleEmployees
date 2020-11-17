feedda913e35f444a083e00d556b0773a9d7e9fe
Richard Gustavsson <retg94@gmail.com>
2020-11-17 17:59:33 +0100

    my first commit
    started with project

---
+++ AdminApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Library;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        public static bool Running = true;
+        static void Main(string[] args)
+        {
+         
+            while(Running)
+            PrintMainMenu();
+        }
+        static void PrintMainMenu()
+            {
+                Console.Clear();
+                Console.WriteLine("ADMIN Mainmenu");
+                Console.WriteLine("0. Exit.");
+                Console.WriteLine("1. Add new employee");
+                int input = Verify.StringToInt(Console.ReadLine());
+                switch(input)
+                {
+                    case 1:
+                        Console.WriteLine("Adding new employee.");
+                        Console.ReadKey();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exit program..");
+                        Console.ReadKey();
+                        Running = false;
+                        break;
+
+                }
+            }
+    }
+
+    
+}
---
+++ Library/Verify.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Library
+{
+    public class Verify
+    {
+        public static int StringToInt(string input)
+        {
+            bool Check = int.TryParse(input, out int result);
+            while (!Check)
+            {
+                Check = int.TryParse(input, out result);
+            }
+            return result;
+        }
+    }
+}
